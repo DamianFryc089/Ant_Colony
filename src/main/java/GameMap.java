c2692e1db1c4979bc822497cec9ca4898bf3233c
Damian Fryc <281089@student.pwr.edu.pl>
2024-05-19 20:15:39 +0200

    Dodanie mapy zapachów, wraz z aktualizacją przy zmianie, dodanie funkcji zmieniejszających wartości zapachu globalnie
    Signed-off-by: Damian Fryc <281089@student.pwr.edu.pl>

--- src/main/java/GameMap.java
+++ src/main/java/GameMap.java
@@ -5,21 +5,48 @@ import java.util.Random;
 public class GameMap {
 	private int width, height;
 	Tile[][] tiles;
-	BufferedImage image;
+	private BufferedImage backgroundImage, scentImage;
 
-	GameMap(){};
-	BufferedImage getImage() {
-		return image;
+	public class Tile{
+		int x, y;
+		private int scentValue;
+		Object cellOccupant;
+		Color tileColor;
+
+		Tile(int x, int y) {
+			this.x = x;
+			this.y = y;
+			scentValue = 0;
+			cellOccupant = null;
+		}
+		void setTileColor(Color newTileColor) {tileColor = newTileColor;}
+		int getScentValue() {return scentValue;}
+		void increaseScentValue(int value) {
+			scentValue+=value;
+			scentImage.setRGB(x, y,
+					new Color(166, 31, 174, Math.min(scentValue,255)).getRGB()
+			);
+		}
+		void decreaseScentValue(int value) {
+			scentValue-=value;
+			if(scentValue<0) scentValue = 0;
+			scentImage.setRGB(x, y,
+					new Color(166, 31, 174, Math.min(scentValue,255)).getRGB()
+			);
+		}
 	}
 
 	void generateMap(Random random, int width, int height) {
 		this.width = width;
 		this.height = height;
 		tiles = new Tile[width][height];
+		backgroundImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		scentImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				tiles[x][y] = new Tile(x, y);
+				tiles[x][y].setTileColor(new Color(255, 255, 255));
 //				tiles[x][y].setTileColor(new Color(31, 135, 23));
 
 					// W teorii żeby mapa nie była zbyt jałowa powinno być tło trochę losowe,
@@ -52,34 +79,27 @@ public class GameMap {
 								37 + random.nextInt(minRand,maxRand-minRand)));
 						break;
 				}*/
-				tiles[x][y].setTileColor(new Color(255, 255, 255));
 			}
 		}
 		generateImage();
 	}
 
 	void generateImage(){
-		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y < height; y++){
-				image.setRGB(x, y, tiles[x][y].tileColor.getRGB());
+				backgroundImage.setRGB(x, y, tiles[x][y].tileColor.getRGB());
 			}
 		}
 	}
 
-	public class Tile{
-		int x, y, scentValue;
-		Object cellOccupant;
-		Color tileColor;
-
-		Tile(int x, int y) {
-			this.x = x;
-			this.y = y;
-			scentValue = 0;
-			cellOccupant = null;
-		}
-		void setTileColor(Color newTileColor) {tileColor = newTileColor;}
+	BufferedImage getBackgroundImage() {
+		return backgroundImage;
 	}
+
+	BufferedImage getScentImage() {
+		return scentImage;
+	}
+
 
 	void takeObject(Object objectToTake){
 		for(int x = 0; x < objectToTake.getSize(); x++){
@@ -93,6 +113,14 @@ public class GameMap {
 		for(int x = 0; x < objectToPlace.getSize(); x++){
 			for(int y = 0; y < objectToPlace.getSize(); y++){
 				tiles[objectToPlace.getX()+x][objectToPlace.getY()+y].cellOccupant = objectToPlace;
+			}
+		}
+	}
+
+	void decreaseScentValues(int value) {
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				tiles[x][y].decreaseScentValue(value);
 			}
 		}
 	}
