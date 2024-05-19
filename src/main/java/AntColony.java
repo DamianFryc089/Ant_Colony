d10d39f03c020a6acb7bbf866a547ec430c507d3
Damian Fryc <281089@student.pwr.edu.pl>
2024-05-19 19:22:58 +0200

    Zmiana nazwy i dodanie zastosowania AntNest
    Signed-off-by: Damian Fryc <281089@student.pwr.edu.pl>

--- src/main/java/AntColony.java
+++ src/main/java/AntColony.java
@@ -4,26 +4,25 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Ant_Colony extends JPanel {
+public class AntColony extends JPanel {
     private Random random;
     private ArrayList<Object> objects;
     private Display display;
     private GameMap gameMap;
 
-    public Ant_Colony(String[] args)
-    {
+    public AntColony(String[] args) {
         objects = new ArrayList<>();
         gameMap = new GameMap();
         display = new Display(args, objects, gameMap);
         random = new Random(3);
         int size = 1;//random.nextInt(1,20);
 
-
         gameMap.generateMap(random, display.getWidth(), display.getHeight());
+        objects.add(new AntNest(100, 100, 25, random, gameMap, objects));
         for (int i = 0; i < 100 ; i++) {
             int wid = random.nextInt(10, gameMap.getWidth() - 10);
             int hei = random.nextInt(10, gameMap.getHeight() - 10);
-            objects.add(new Testowy_Kwadrat(wid, hei, size, random, gameMap));
+            objects.add(new Ant(wid, hei, size, random, gameMap));
         }
 
 //         Timer z pętlą symulacji na innym wątku
@@ -40,6 +39,6 @@ public class Ant_Colony extends JPanel {
     }
 
     public static void main(String[] args) {
-        Ant_Colony simulation = new Ant_Colony(args);
+        AntColony simulation = new AntColony(args);
     }
 }
